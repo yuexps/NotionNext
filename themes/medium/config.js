d8c09a35cb58bd38a00d64a6bff6eb306804978d
yuexps <69074603+yuexps@users.noreply.github.com>
2024-03-15 01:22:17 +0800

    Update config.js

--- themes/medium/config.js
+++ themes/medium/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   // Style
   MEDIUM_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MEDIUM_RIGHT_DARK || false, // 右侧面板深色模式
 
-  MEDIUM_POST_LIST_COVER: true, // 文章列表显示图片封面
+  MEDIUM_POST_LIST_COVER: false, // 文章列表显示图片封面
   MEDIUM_POST_LIST_PREVIEW: true, // 列表显示文章预览
   MEDIUM_POST_LIST_CATEGORY: true, // 列表显示文章分类
   MEDIUM_POST_LIST_TAG: true, // 列表显示文章标签
