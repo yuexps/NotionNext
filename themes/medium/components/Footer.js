af7bbed2733c9de0e0a1d482619b62bc40d6e1b2
yuexps <69074603+yuexps@users.noreply.github.com>
2024-03-15 01:28:56 +0800

    Update Footer.js

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -20,7 +20,6 @@ const Footer = ({ title }) => {
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <br/>
     </footer>
   )
 }
